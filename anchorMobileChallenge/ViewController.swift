f794e30b089537f00108affc96208bad7127e5af
Alwin Ventura <amventura515@gmail.com>
2019-06-09 23:44:39 -0400

    title names display in tableview

--- anchorMobileChallenge/ViewController.swift
+++ anchorMobileChallenge/ViewController.swift
@@ -7,7 +7,7 @@
 //MVP:
 // 1) retrive data from given endpoint URL "https://s3-us-west-2.amazonaws.com/anchor-website/challenges/bsb.json"
 // 2) preset data to user as a list of items as a Table
-// 3) each cell should display title adn toggle-able play/pause button
+// 3) each cell should display title and toggle-able play/pause button
 // 4) user has to be able to tap on item to listen to playback
 // 5) if user taps on a song and audio is already playing, then other audio should stop
 // 6) when audio finishes playing, it should go to the next track
@@ -25,33 +25,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     struct Tracks: Decodable {
 
-        let title: String?
-        let mediaUrl: String?
-        let imageUrl: String?
-        let duration: Int?
+        let title: String
+        let mediaUrl: String
+        let imageUrl: String
+        let duration: Int
     }
     
     let dataArray: NSArray = ["First", "Second", "Third"]
     var myTableView: UITableView!
+    private var tracks = [Tracks]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //downloading JSON data
         let jsonUrlString = "https://s3-us-west-2.amazonaws.com/anchor-website/challenges/bsb.json"
         guard let url = URL(string: jsonUrlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             
             guard let data = data else { return }
-            
             do {
+                let downloadedTracks = try JSONDecoder().decode(TracksJson.self, from: data)
+                self.tracks = downloadedTracks.tracks
+                DispatchQueue.main.async {
+                    self.myTableView.reloadData()
+                }
                 
-                let tracksJson = try JSONDecoder().decode(TracksJson.self, from: data)
-                print(tracksJson.tracks)
-                
+                print(downloadedTracks.tracks)
             } catch let jsonErr {
                 print("Error serializing json:", jsonErr)
             }
-
             }.resume()
         
         //MARK : Displaying the Tableview 
@@ -60,23 +63,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let displayHeight: CGFloat = self.view.frame.height
         
         myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight-barHeight))
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "TracksCell")
         myTableView.dataSource = self
         myTableView.delegate = self
         self.view.addSubview(myTableView)
+        myTableView.reloadData()
     }
     
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        print("I'm selecting \(indexPath.row)")
-    }
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        print("I'm selecting \(indexPath.row)")
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return tracks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
-        cell.textLabel!.text = "\(dataArray[indexPath.row])"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TracksCell", for: indexPath as IndexPath)
+
+        if let storedImage = URL(string: tracks[indexPath.row].imageUrl) {
+            DispatchQueue.global().async {
+                let data = try? Data(contentsOf: storedImage)
+                if let data = data {
+                    let image = UIImage(data: data)
+                    DispatchQueue.main.async {
+                        cell.imageView?.image = image
+                    }
+                }
+            }
+        }
+        
+        cell.textLabel!.text = "\(tracks[indexPath.row].title)"
         return cell
     }
 }
